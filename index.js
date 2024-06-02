48f92d50dd7aaeee07ccdf3c166bf68a4e45fbcd
daido23 <bbialy585@gmail.com>
2024-06-02 14:34:43 +0200

    t

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-razrazraz
+const { Client, Events, GatewayIntentBits } = require("discord.js");
+
+require('dotenv-flow').config();
+
+const config = {
+    token: process.env.DISCORD_TOKEN
+
+}
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.once(Events.ClientReady, readyclient => {
+    console.log(`Ready! Logged as ${readyclient.user.tag} `);
+})
+
+client.login(config.token);
