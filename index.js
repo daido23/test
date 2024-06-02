ed1cf93febaca0515f14818a043223823c9eb6e7
daido23 <bbialy585@gmail.com>
2024-06-02 14:42:31 +0200

    1

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ const config = {
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 client.once(Events.ClientReady, readyclient => {
     console.log(`Ready! Logged as ${readyclient.user.tag} `);
-})
+})s231
 
 client.login(config.token);
