7b4153a28153e5c00fc4e4ba1f18f56b5aa86717
daido23 <bbialy585@gmail.com>
2024-06-02 15:32:08 +0200

    2

--- commands/utility/Ping.js
+++ commands/utility/Ping.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
-    .setName("Ping")
+    .setName("ping")
     .setDescription("Replies with pong"),
     async execute(interaction) {
         await interaction.reply('Pong')
